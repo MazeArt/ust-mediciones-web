7b1cb35af0c44fe387200d4050b070abda18800a
fcomarshall-kauel <fcoarturo.marshall@gmail.com>
2024-05-14 19:53:23 -0400

    Funcionalidad "Tarjeta Bloqueada"

--- app/Components/Tarjeta.js
+++ app/Components/Tarjeta.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { FaLock } from 'react-icons/fa';
 
-const Tarjeta = ({ image, title, description }) => {
+const Tarjeta = ({ image, title, description, locked }) => {
   return (
-    <div className="bg-gray-900 text-center p-6 rounded-xl border-4 border-white transition-transform transform hover:scale-105 hover:border-emerald-400">
-      <div
-        className="h-40 mb-4 bg-gray-700"
-        style={{
-          backgroundImage: `url(${image})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      ></div>
+    <div className={`relative bg-gray-900 text-center p-6 rounded-xl border-4 transition-transform transform hover:scale-105 ${locked ? 'hover:border-red-400' : 'hover:border-emerald-400'}`}>
+      {locked && (
+        <div className="absolute inset-0 bg-black bg-opacity-50 rounded-xl ">
+     
+        </div>
+        
+      )}
+      <div className="relative h-40 mb-4 bg-gray-700 rounded-xl " style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        {locked && (
+          <div className="absolute inset-0 bg-black bg-opacity-70 flex flex-col justify-center items-center">
+            <FaLock className="text-white text-3xl" />
+            <span className="text-white text-lg opacity-0 hover:opacity-100">Desbloquea el Tutorial</span>
+       
+          </div>
+        )}
+      </div>
       <h3 className="text-emerald-400 text-lg font-semibold">{title}</h3>
       <p className="text-gray-400">{description}</p>
     </div>
@@ -18,5 +26,4 @@ const Tarjeta = ({ image, title, description }) => {
 };
 
 export default Tarjeta;
-
 {/*------- color de fondo/ centrado /p: padding/ redondeado / grosor del borde / color del borde */}
--- app/intro/page.js
+++ app/intro/page.js
@@ -3,70 +3,83 @@ import Tarjeta from '../Components/Tarjeta';
 //import Tarjeta2 from '../Components/Tarjeta2';
 
 
-function IndexPage() {
+function IndexPage()
+{
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
       {/*ENCABEZADO PRINCIPAL*/}
-      <br/>
-      <br/>
+      <br />
+      <br />
       <h1 className="text-5xl text-center font-bold text-emerald-400">
-      BIENVENIDO A LA EXPERIENCIA</h1>
+        BIENVENIDO A LA EXPERIENCIA</h1>
       <h1 className="text-5xl text-center font-semibold text-white">
-      VIRTUAL DE MEDICIONES
+        VIRTUAL DE MEDICIONES
       </h1>
       <div>
-      <br/>   
-        <br/>
+        <br />
+        <br />
 
-      <h2 className="text-2xl text-white mb-4 font-bold text-gray-800 bg-emerald-400 px-4 mb-5 rounded-xl">
-        Tus primeros pasos para comenzar
-      </h2></div>
+        <h2 className="text-2xl text-white mb-4 font-bold text-gray-800 bg-emerald-400 px-4 mb-5 rounded-xl">
+          Tus primeros pasos para comenzar
+        </h2></div>
       <br />
-<div className="flex flex-row items-center gap-6 justify-center bg-gray-800">
-      {/*CONTENEDOR TUTORIAL*/}
+      <div className="flex flex-row items-center gap-6 justify-center bg-gray-800">
+        {/*CONTENEDOR TUTORIAL*/}
 
-      <Tarjeta image='/tutorial.png'
-          title='Tutorial' description='Realización de mediciones mediante el uso de un contador Geiger'></Tarjeta>
-          
+        <Tarjeta image='/tutorial.png'
+          title='Tutorial'
+          description='Realización de mediciones mediante el uso de un contador Geiger'
+          locked={false}></Tarjeta>
 
-          {/*CONTENEDOR EXTINTORES*/}
+
+        {/*CONTENEDOR EXTINTORES*/}
 
         <Tarjeta image='/Extintores.png'
-          title='Extintores' description='Realización de uso de extintores ante una situación de emergencia'></Tarjeta>
+          title='Extintores'
+          description='Realización de uso de extintores ante una situación de emergencia'
+          locked={false}></Tarjeta>
 
-</div>
+      </div>
 
       {/*SUBENCABEZADO*/}
       <br />
       <br />
       <h2 className="text-2xl text-white mb-4 font-bold text-gray-800 bg-emerald-400 px-4 mb-5 rounded-xl">
-     Selecciona una experiencia
+        Selecciona una experiencia
       </h2>
       <br />
       {/*CONTENEDOR DE OPCIONES*/}
       {/*------- -----grid: grilla / Cols: columna/ md:pantalla mediana / lg pantalla grande*/}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-4 md:px-8 lg:px-16" >
-        
+
         {/*CONTENEDOR EXP 1*/}
 
         <Tarjeta image='/Radiacion-ionizante.png'
-          title='Radiación Ionizante' description='Realización de mediciones mediante el uso de un contador Geiger'></Tarjeta>
+          title='Radiación Ionizante'
+          description='Realización de mediciones mediante el uso de un contador Geiger'
+          locked={true}></Tarjeta>
 
 
-         {/*CONTENEDOR EXP 2*/}
+        {/*CONTENEDOR EXP 2*/}
 
-         <Tarjeta image='/Espacio-Confinado.png'
-          title='Espacio Confinado' description='Mediciones de luxometrías, mediante el uso de un luxómetro como herramienta de medición'></Tarjeta>
+        <Tarjeta image='/Espacio-Confinado.png'
+          title='Espacio Confinado'
+          description='Mediciones de luxometrías, mediante el uso de un luxómetro como herramienta de medición'
+          locked={false}></Tarjeta>
 
         {/*CONTENEDOR EXP 3*/}
 
-         <Tarjeta image='/Laboratorio-Quimico.png'
-          title='Laboratorio Químico' description='Experiencia de medición con la herramienta del multigas, pasando por distintas fuentes de gases'></Tarjeta>
-      
+        <Tarjeta image='/Laboratorio-Quimico.png'
+          title='Laboratorio Químico' 
+          description='Experiencia de medición con la herramienta del multigas, pasando por distintas fuentes de gases'
+          locked={true}></Tarjeta>
+
         {/*CONTENEDOR EXP 4*/}
 
-          <Tarjeta image='/Laboratorio-Minero.png'
-          title='Laboratorio Minero' description='Experiencia de medición con la herramienta del multigas, pasando por distintas fuentes de gases'></Tarjeta>
+        <Tarjeta image='/Laboratorio-Minero.png'
+          title='Laboratorio Minero' 
+          description='Experiencia de medición con la herramienta del multigas, pasando por distintas fuentes de gases'
+          locked={false}></Tarjeta>
 
       </div>
     </div>
